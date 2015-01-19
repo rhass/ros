a02f5e3a838944b86f7da88f4d0a98c9d56d2dda
Ryan Hass <ryan@invalidchecksum.net>
2015-01-18 23:18:24 -0800

    Add source and issues url for supermarket

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,12 @@
 name             'ros'
 maintainer       'Ryan Hass'
 maintainer_email 'ryan@invalidchecksum.net'
+source_url       'https://github.com/rhass/ros'
+issues_url       'https://github.com/rhass/ros/issues'
 license          'Apache 2.0'
 description      'Installs/Configures ros'
 long_description 'Installs/Configures ros'
-version          '0.1.0'
+version          '0.1.1'
 
 supports 'ubuntu'
 
