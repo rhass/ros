9b97023ea4e9a00fa560d362c8ead30c494cca09
Ryan Hass <ryan_hass@rapid7.com>
2015-02-03 08:12:17 -0800

    Remove source_url and issue_url
    Berkshelf freaks out when it sees the two directives in the metadata
    file.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,6 @@
 name             'ros'
 maintainer       'Ryan Hass'
 maintainer_email 'ryan@invalidchecksum.net'
-source_url       'https://github.com/rhass/ros'
-issues_url       'https://github.com/rhass/ros/issues'
 license          'Apache 2.0'
 description      'Installs/Configures ros'
 long_description 'Installs/Configures ros'
