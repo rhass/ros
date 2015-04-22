6f320670ba1796bc27c61bf61af972347e2c8f7f
Ryan Hass <ryan_hass@rapid7.com>
2015-04-22 11:54:54 -0700

    Fix serverspec test for catkin_package for Jade

--- test/integration/ros_jade/serverspec/catkin_package_spec.rb
+++ test/integration/ros_jade/serverspec/catkin_package_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe 'catkin_package resource' do
   it 'clones the git repository' do
-    expect(file('/opt/catkin_ws/src/xv_11_laser_driver/.git')).to be_directory
+    expect(file('/opt/catkin_ws/src/roslint/.git')).to be_directory
   end
 
   subject(:catkin_find) { command('. /opt/ros/jade/setup.sh ; catkin_find xv11_laser_driver') }
   it 'builds the catkin package with catkin_make' do
     expect(file('/opt/catkin_ws/build')).to be_directory
-    expect(file('/opt/catkin_ws/build/xv_11_laser_driver')).to be_directory
+    expect(file('/opt/catkin_ws/build/roslint')).to be_directory
     expect(catkin_find.exit_status).to eq(0)
   end
 end
